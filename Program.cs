17487b94b65ad42fa0858f8e8737648b9b69db7a
Daredevil <aliyor.0276@gmail.com>
2023-08-29 20:36:10 +0500

    Task 25

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int A = 2; // замените на нужное значение
+int B = 3; // замените на нужное значение
+int result = 1;
+
+for (int i = 0; i < B; i++)
+{
+    result *= A;
+}
+
+Console.WriteLine("{0} в степени {1} равно {2}", A, B, result);
